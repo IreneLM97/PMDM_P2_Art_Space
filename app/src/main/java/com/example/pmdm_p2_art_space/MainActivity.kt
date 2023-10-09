03f86d4132421e881d3e05876598236968c556f1
IreneLM97 <irenelm1997@gmail.com>
2023-10-09 12:42:38 +0200

    Agregadas descripciones de obras y funcionalidad de botones anterior y siguiente

--- app/src/main/java/com/example/pmdm_p2_art_space/MainActivity.kt
+++ app/src/main/java/com/example/pmdm_p2_art_space/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -18,9 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pmdm_p2_art_space.ui.theme.PMDM_P2_Art_SpaceTheme
 
@@ -41,41 +49,98 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+class ArtData(
+    val imageResource: Int,
+    val titleResource: Int,
+    val authorResource: Int,
+    val yearResource: Int
+);
 
+@Composable
+fun GetArtData(result:Int = 1): ArtData {
+    return when(result) {
+        1 -> ArtData(R.drawable.obra_1, R.string.title_1, R.string.author_1, R.string.year_1)
+        2 -> ArtData(R.drawable.obra_2, R.string.title_2, R.string.author_2, R.string.year_2)
+        3 -> ArtData(R.drawable.obra_3, R.string.title_3, R.string.author_3, R.string.year_3)
+        4 -> ArtData(R.drawable.obra_4, R.string.title_4, R.string.author_4, R.string.year_4)
+        else -> ArtData(R.drawable.obra_5, R.string.title_5, R.string.author_5, R.string.year_5)
+    }
+}
 
 @Composable
 fun ArtSpaceCarousel(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf(1) }
-    val imageResource = when(result) {
-        1 -> R.drawable.obra_1
-        2 -> R.drawable.obra_2
-        3 -> R.drawable.obra_3
-        4 -> R.drawable.obra_4
-        else -> R.drawable.obra_5
-    }
+    val numImages = 5
+
+    val artData = GetArtData(result)
+
     Column (
-        modifier = modifier,
+        modifier = Modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = imageResource),
-            contentDescription = result.toString()
-        )
-        Button(
-            onClick = { result = (1..6).random()}
+        Row(
+            modifier = Modifier.padding(30.dp),
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(
-                stringResource(R.string.anterior),
-                fontSize = 24.sp
+                text = "ART SPACE",
+                fontSize = 50.sp
             )
         }
-        Button(
-            onClick = { result = (1..6).random()}
+        Row {
+            Image(
+                painter = painterResource(artData.imageResource),
+                contentDescription = result.toString(),
+                modifier = Modifier.size(400.dp)
+            )
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(3.dp)
+                .background(colorResource(R.color.purple_200)),
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(
-                stringResource(R.string.siguiente),
-                fontSize = 24.sp
+                text = stringResource(artData.titleResource),
+                fontSize = 25.sp
             )
+            Text(
+                text = stringResource(artData.authorResource),
+                fontSize = 23.sp
+            )
+            Text(
+                text = stringResource(artData.yearResource),
+                fontSize = 23.sp
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 30.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    result = (result - 1 + numImages) % numImages
+                }
+            ) {
+                Text(
+                    stringResource(R.string.previous),
+                    fontSize = 24.sp
+                )
+            }
+            Button(
+                onClick = {
+                    result = (result + 1) % numImages
+                }
+            ) {
+                Text(
+                    stringResource(R.string.next),
+                    fontSize = 24.sp
+                )
+            }
         }
     }
 }
